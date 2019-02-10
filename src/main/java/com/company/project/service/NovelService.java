520af927117597d7784e73ec57c18438fa556fc1
yefei <2360274407@qq.com>
2019-02-10 15:33:17 +0800

    更正代理设置

--- src/main/java/com/company/project/service/NovelService.java
+++ src/main/java/com/company/project/service/NovelService.java
@@ -7,13 +7,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.company.project.core.AbstractService;
 import com.company.project.core.JsonResult;
 import com.company.project.dao.NovelMapper;
@@ -21,7 +18,6 @@ import com.company.project.downloader.DingDianDownloader;
 import com.company.project.model.Chapter;
 import com.company.project.model.Novel;
 import com.company.project.util.Downloader;
-
 import tk.mybatis.mapper.entity.Condition;
 import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.ResultItems;
@@ -39,108 +35,114 @@ import us.codecraft.webmagic.proxy.SimpleProxyProvider;
 @Service
 @Transactional
 public class NovelService extends AbstractService<Novel> {
-    @Resource
-    private NovelMapper novelMapper;
-    
-    @Autowired
-    private ChapterService chapterService;
+  @Resource
+  private NovelMapper novelMapper;
 
-    public JsonResult saveChaptersByUrl(Novel novel) {
-    	List<Chapter> chapters=Downloader.downloadChapters(novel.getUrl());
-    	if(chapters.size()==0) {
-    		return JsonResult.failure("下载失败");
-    	}
-    	chapters.forEach(s->{s.setNovelid(novel.getId());});
-    	chapterService.save(chapters);
-    	return JsonResult.success();
-    }
-    
-    public JsonResult updateNovelDetail(Novel novel) {
-    	Date startTime=new Date();
-    	List<Chapter> chapters=this.findNeedDownload(novel);
-    	ExecutorService service=Executors.newFixedThreadPool(16);
-    	for(Chapter chapter:chapters) {
-    		service.submit(()->{
-    			if(Downloader.downloadDetail(chapter)) {
-    				chapterService.update(chapter);
-    			}
-    		});
-    	}
-    	service.shutdown();
-    	while(true) {
-    		if(service.isTerminated()) {
-    			Date endTime=new Date();
-    			SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    			System.out.println("开始时间是"+sdf.format(startTime));
-    			System.out.println("结束时间是"+sdf.format(endTime));
-    			return JsonResult.success();
-    		}
-    		try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-    	}
-    }
-    
-    public List<Chapter> findNeedDownload(Novel novel){
-    	Condition con=new Condition(Chapter.class);
-    	con.and().andIsNull("content").andEqualTo("novelid", novel.getId());
-    	List<Chapter> chapters=chapterService.findByCondition(con);
-    	return chapters;
-    }
-    
-    public List<Map<String,Object>> findChaptersByNovelId(Integer novelId){
-    	return novelMapper.findChaptersByNovelId(novelId);
-    }
-    
-    public void download() {
-//    	String url="http://www.x23us.com/html/69/69957/30372903.html";
-//    	Chapter chapter=chapterService.findById(12);
-//    	Condition con=new Condition(Chapter.class);
-//    	con.and().andEqualTo("url",url);
-//    	Chapter c=chapterService.findByCondition(con).get(0);
-//    	System.out.println(c);;
-//    	System.out.println(chapter);
-    	DingDianDownloader dddownloader=new DingDianDownloader();
-		HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
-    	SimpleProxyProvider proxyProvider = SimpleProxyProvider.from(new Proxy("proxysz.aac.com", 80));
-    	httpClientDownloader.setProxyProvider(proxyProvider);
-    	List<Request> requests=new ArrayList<>();
-    	Spider spider=Spider.create(dddownloader).thread(32).setDownloader(httpClientDownloader).addPipeline(new MysqlPipeline());
-    	for(int i=1;i<=10;i++) {
-    		Request request=new Request("https://www.x23us.com/modules/article/search.php?searchtype=keywords&searchkey=%C8%FD%B9%FA&page="+i);
-    		request.putExtra("type", "novels");
-    		requests.add(request);
-    		spider.addRequest(request);
-    	}
-    	spider.run();
-    }
-    
-    class MysqlPipeline implements Pipeline{
+  @Autowired
+  private ChapterService chapterService;
 
-		@Override
-		public void process(ResultItems resultItems, Task task) {
-			if("novels".equals(resultItems.getRequest().getExtra("type"))) {
-				List<Novel> novels=resultItems.get("novels");
-				save(novels);
-			}else if("chapters".equals(resultItems.getRequest().getExtra("type"))) {
-				List<Chapter> chapters=resultItems.get("chapters");
-				Novel novel=findBy("url", resultItems.getRequest().getExtra("novelUrl"));
-				chapters.forEach(s->{
-					s.setNovelid(novel.getId());
-				});
-				chapterService.save(chapters);
-			}else if("chapterDetail".equals(resultItems.getRequest().getExtra("type"))) {
-				String content=resultItems.get("content");
-				Condition con=new Condition(Chapter.class);
-		    	con.and().andEqualTo("url",resultItems.getRequest().getUrl());
-		    	Chapter chapter=chapterService.findByCondition(con).get(0);
-				chapter.setContent(content);
-				chapterService.update(chapter);
-			}
-			
-		}
-    	
+  public JsonResult saveChaptersByUrl(Novel novel) {
+    List<Chapter> chapters = Downloader.downloadChapters(novel.getUrl());
+    if (chapters.size() == 0) {
+      return JsonResult.failure("下载失败");
     }
+    chapters.forEach(s -> {
+      s.setNovelid(novel.getId());
+    });
+    chapterService.save(chapters);
+    return JsonResult.success();
+  }
+
+  public JsonResult updateNovelDetail(Novel novel) {
+    Date startTime = new Date();
+    List<Chapter> chapters = this.findNeedDownload(novel);
+    ExecutorService service = Executors.newFixedThreadPool(16);
+    for (Chapter chapter : chapters) {
+      service.submit(() -> {
+        if (Downloader.downloadDetail(chapter)) {
+          chapterService.update(chapter);
+        }
+      });
+    }
+    service.shutdown();
+    while (true) {
+      if (service.isTerminated()) {
+        Date endTime = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("开始时间是" + sdf.format(startTime));
+        System.out.println("结束时间是" + sdf.format(endTime));
+        return JsonResult.success();
+      }
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public List<Chapter> findNeedDownload(Novel novel) {
+    Condition con = new Condition(Chapter.class);
+    con.and().andIsNull("content").andEqualTo("novelid", novel.getId());
+    List<Chapter> chapters = chapterService.findByCondition(con);
+    return chapters;
+  }
+
+  public List<Map<String, Object>> findChaptersByNovelId(Integer novelId) {
+    return novelMapper.findChaptersByNovelId(novelId);
+  }
+
+  public void download() {
+    // String url="http://www.x23us.com/html/69/69957/30372903.html";
+    // Chapter chapter=chapterService.findById(12);
+    // Condition con=new Condition(Chapter.class);
+    // con.and().andEqualTo("url",url);
+    // Chapter c=chapterService.findByCondition(con).get(0);
+    // System.out.println(c);;
+    // System.out.println(chapter);
+    DingDianDownloader dddownloader = new DingDianDownloader();
+    HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+    SimpleProxyProvider proxyProvider =
+        SimpleProxyProvider.from(new Proxy("proxyhk.aac.com", 8011));
+    httpClientDownloader.setProxyProvider(proxyProvider);
+    List<Request> requests = new ArrayList<>();
+    Spider spider = Spider.create(dddownloader).thread(32).setDownloader(httpClientDownloader)
+        .addPipeline(new MysqlPipeline());
+    for (int i = 1; i <= 1; i++) {
+      Request request = new Request(
+          "https://www.x23us.com/modules/article/search.php?searchtype=keywords&searchkey=%C8%FD%B9%FA&page="
+              + i);
+      request.putExtra("type", "novels");
+      requests.add(request);
+      spider.addRequest(request);
+    }
+    spider.run();
+  }
+
+  class MysqlPipeline implements Pipeline {
+
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+      if ("novels".equals(resultItems.getRequest().getExtra("type"))) {
+        List<Novel> novels = resultItems.get("novels");
+        save(novels);
+      } else if ("chapters".equals(resultItems.getRequest().getExtra("type"))) {
+        List<Chapter> chapters = resultItems.get("chapters");
+        Novel novel = findBy("url", resultItems.getRequest().getExtra("novelUrl"));
+        chapters.forEach(s -> {
+          s.setNovelid(novel.getId());
+        });
+        chapterService.save(chapters);
+      } else if ("chapterDetail".equals(resultItems.getRequest().getExtra("type"))) {
+        String content = resultItems.get("content");
+        Condition con = new Condition(Chapter.class);
+        con.and().andEqualTo("url", resultItems.getRequest().getUrl());
+        Chapter chapter = chapterService.findByCondition(con).get(0);
+        chapter.setContent(content);
+        chapterService.update(chapter);
+      }
+
+    }
+
+  }
 }
--- src/main/java/com/company/project/web/ApiController.java
+++ src/main/java/com/company/project/web/ApiController.java
@@ -3,12 +3,10 @@ package com.company.project.web;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.company.project.core.JsonResult;
 import com.company.project.model.Chapter;
 import com.company.project.model.Novel;
@@ -19,31 +17,31 @@ import com.company.project.service.NovelService;
 @RequestMapping("/api")
 public class ApiController {
 
-	@Autowired
-	private NovelService novelService;
-	
-	@Autowired
-	private ChapterService chapterService;
-	
-	@GetMapping("/bookList")
-	public JsonResult bookList() {
-		return JsonResult.success(novelService.findAll());
-	}
-	
-	@GetMapping("/chapterList")
-	public JsonResult chapterList(Integer novelId) {
-		Objects.requireNonNull(novelId, "novelId is null");
-		Novel novel=novelService.findById(novelId);
-		Map<String,Object> result=new HashMap<>();
-		result.put("novel", novel.getName());
-		result.put("chapters", novelService.findChaptersByNovelId(novelId));
-		return JsonResult.success(result);
-	}
-	
-	@GetMapping("/chapterDetail")
-	public JsonResult chapterDetail(Integer chapterId) {
-		Objects.requireNonNull(chapterId, "chapterId is null");
-		Chapter c=chapterService.findById(chapterId);
-		return JsonResult.success(c);
-	}
+  @Autowired
+  private NovelService novelService;
+
+  @Autowired
+  private ChapterService chapterService;
+
+  @GetMapping("/bookList")
+  public JsonResult bookList() {
+    return JsonResult.success(novelService.findAll());
+  }
+
+  @GetMapping("/chapterList")
+  public JsonResult chapterList(Integer novelId) {
+    Objects.requireNonNull(novelId, "novelId is null");
+    Novel novel = novelService.findById(novelId);
+    Map<String, Object> result = new HashMap<>();
+    result.put("novel", novel.getName());
+    result.put("chapters", novelService.findChaptersByNovelId(novelId));
+    return JsonResult.success(result);
+  }
+
+  @GetMapping("/chapterDetail")
+  public JsonResult chapterDetail(Integer chapterId) {
+    Objects.requireNonNull(chapterId, "chapterId is null");
+    Chapter c = chapterService.findById(chapterId);
+    return JsonResult.success(c);
+  }
 }
